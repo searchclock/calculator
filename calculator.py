1a5ba3eaaca9a0e6bf4358456a8b813b71f8753a
searchclock <65318863+searchclock@users.noreply.github.com>
2020-05-14 16:21:28 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+"""
+you can use this program as a calculator by
+changing the numbers in the print statement.
+always remember though, all 4 numbers are
+important, especially the last one.
+"""
+print (" Search clock calculator TM")
+
+print ("*=X, /=divided by, +=+, ===, -=-.")
+
+eq ="12345*4325"
+print (eq,"=",eval(eq))
